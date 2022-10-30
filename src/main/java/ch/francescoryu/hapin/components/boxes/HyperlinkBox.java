1ddb185341a36f2aed33b6b56d1afd65e6597b08
francescoryu <francescoryu04@gmail.com>
2022-10-31 00:21:27 +0100

    changed to tab view

---
+++ src/main/java/ch/francescoryu/hapin/components/boxes/HyperlinkBox.java
@@ -0,0 +1,215 @@
+package ch.francescoryu.hapin.components.boxes;
+
+import ch.francescoryu.hapin.DataHandler;
+import ch.francescoryu.hapin.Menu;
+import ch.francescoryu.hapin.buttonMethods.MenuMethods;
+import ch.francescoryu.hapin.components.buttons.AddButton;
+import ch.francescoryu.hapin.components.buttons.DeleteButton;
+import ch.francescoryu.hapin.popups.ErrorPopup;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class HyperlinkBox extends VBox {
+
+    private final ArrayList<Button> buttons = new ArrayList<>();
+
+    TextField inputButtonName;
+    TextField inputButtonUrl;
+
+    Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+
+    public HyperlinkBox() {
+        Label linksText = new Label("Hyperlinks");
+        linksText.setStyle("-fx-font-size: 30; " +
+                "-fx-font-family: 'Microsoft Sans Serif'; " +
+                "-fx-text-alignment: center;" +
+                "-fx-text-fill: #cfcfcf;");
+
+        //--------------------------------------------------------------------------------------------------------------
+
+
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        HBox infoBox = new HBox(3);
+        infoBox.setStyle("-fx-alignment: center-right; -fx-font-size: 40; -fx-padding: 10; ");
+        infoBox.getChildren().addAll(DataHandler.createClock());
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        GridPane buttonGridPane = new GridPane();
+        //buttonGridPane.setStyle(" -fx-border-color: red;");
+        buttonGridPane.setAlignment(Pos.TOP_CENTER);
+        buttonGridPane.setVgap(10);
+        //buttonGridPane.setAlignment(Pos.CENTER);
+        buttonGridPane.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+
+        ScrollPane buttonScrollPane = new ScrollPane();
+        buttonScrollPane.setMaxWidth(300);
+        buttonScrollPane.setFitToWidth(true);
+        buttonScrollPane.setContent(buttonGridPane);
+        buttonScrollPane.getStyleClass().addAll(".scroll-bar", ".scroll-pane");
+        buttonScrollPane.setStyle("-fx-background-color: transparent; -fx-background: transparent; -fx-padding: 10");
+
+        AddButton addButton = new AddButton(35);
+
+        DeleteButton deleteButton = new DeleteButton();
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        try {
+            DataHandler.createButtons(buttons, buttonGridPane, true, deleteButton);
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        HBox navButtonBox = new HBox();
+        navButtonBox.setStyle("-fx-alignment: center; -fx-padding: 20");
+        navButtonBox.setSpacing(10);
+        navButtonBox.getChildren().addAll(addButton, deleteButton);
+
+        VBox navBox = new VBox();
+        navBox.setStyle("-fx-padding: 20; -fx-border-color: white");
+        navBox.setPrefHeight(size.getHeight() / 2);
+        navBox.setSpacing(10);
+        navBox.setPrefWidth(size.getWidth() / 3);
+        navBox.setAlignment(Pos.TOP_CENTER);
+        navBox.getChildren().addAll(linksText, buttonScrollPane, navButtonBox);
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        BorderPane popupBorderPane = new BorderPane();
+
+
+
+        Label addButtonLabel = new Label("Add button");
+        MenuMethods.setLabelStyle(addButtonLabel);
+
+        Label buttonNameText = new Label("Button Name: ");
+        MenuMethods.setPopUpStyle(buttonNameText);
+        Label urlText = new Label("URL: ");
+        MenuMethods.setPopUpStyle(urlText);
+        Label imgText = new Label("Add Icon: ");
+        MenuMethods.setPopUpStyle(imgText);
+
+        VBox textBox = new VBox();
+        textBox.setStyle("-fx-padding: 20");
+        textBox.setSpacing(35);
+        textBox.getChildren().addAll(buttonNameText, urlText, imgText);
+
+        inputButtonName = new TextField();
+        inputButtonName.setPrefColumnCount(20);
+        MenuMethods.setInputTextFieldStyle(inputButtonName);
+
+        inputButtonUrl = new TextField();
+        MenuMethods.setInputTextFieldStyle(inputButtonUrl);
+        inputButtonUrl.setPrefColumnCount(20);
+
+        Button chooseFile = new Button("Select File");
+        chooseFile.setCursor(javafx.scene.Cursor.HAND);
+        chooseFile.setStyle("-fx-font-size: 16; -fx-font-family: 'Microsoft Sans Serif'; -fx-background-color: black; -fx-text-fill: #cfcfcf; -fx-border-color: #737373; -fx-border-width: 2");
+
+        chooseFile.setOnAction(actionEvent -> {
+            DataHandler.saveButtonData(DataHandler.getInputFromTextField(inputButtonName, inputButtonUrl));
+        });
+
+        VBox inputBox = new VBox();
+        inputBox.setStyle("-fx-padding: 15");
+        inputBox.setSpacing(24);
+        inputBox.getChildren().addAll(inputButtonName, inputButtonUrl, chooseFile);
+
+        Button saveButton = new Button();
+        saveButton.setCursor(javafx.scene.Cursor.HAND);
+        ImageView saveButtonImageView = new ImageView(new javafx.scene.image.Image(Objects.requireNonNull(ch.francescoryu.hapin.Menu.class.getResourceAsStream("navMenuImg/save.png"))));
+        saveButton.setStyle("-fx-background-color: black; -fx-border-color: #737373; -fx-border-width: 2");
+        saveButtonImageView.setFitHeight(25);
+        saveButtonImageView.setPreserveRatio(true);
+        saveButton.setGraphic(saveButtonImageView);
+
+        Button clearButton = new Button();
+        clearButton.setCursor(javafx.scene.Cursor.HAND);
+        ImageView clearButtonImageView = new ImageView(new javafx.scene.image.Image(Objects.requireNonNull(ch.francescoryu.hapin.Menu.class.getResourceAsStream("navMenuImg/delete.png"))));
+        clearButton.setStyle("-fx-background-color: black; -fx-border-color: #737373; -fx-border-width: 2");
+        clearButtonImageView.setFitHeight(25);
+        clearButtonImageView.setPreserveRatio(true);
+        clearButton.setGraphic(clearButtonImageView);
+
+        clearButton.setOnAction(actionEvent -> {
+            inputButtonName.setText("");
+            inputButtonUrl.setText("");
+        });
+
+        Button cancelButton = new Button();
+        cancelButton.setCursor(Cursor.HAND);
+        ImageView cancelButtonImageView = new ImageView(new Image(Objects.requireNonNull(Menu.class.getResourceAsStream("navMenuImg/cancel.png"))));
+        cancelButton.setStyle("-fx-background-color: black; -fx-border-color: #737373; -fx-border-width: 2");
+        cancelButtonImageView.setFitHeight(25);
+        cancelButtonImageView.setPreserveRatio(true);
+        cancelButton.setGraphic(cancelButtonImageView);
+
+        HBox buttonBox = new HBox();
+        buttonBox.setStyle("-fx-alignment: center; -fx-padding: 10");
+        buttonBox.setSpacing(10);
+        buttonBox.getChildren().addAll(cancelButton, clearButton, saveButton);
+
+        popupBorderPane.setStyle("");
+        popupBorderPane.setLeft(textBox);
+        popupBorderPane.setRight(inputBox);
+        popupBorderPane.setBottom(buttonBox);
+        //popupBorderPane.setMaxSize(300, 200);
+
+        VBox addButtonBox = new VBox();
+        addButtonBox.getChildren().addAll(addButtonLabel, popupBorderPane);
+        addButtonBox.setStyle("-fx-padding: 20; -fx-border-color: #737373; -fx-border-width: 2;");
+        addButtonBox.setAlignment(Pos.CENTER);
+        addButtonBox.setSpacing(10);
+
+        getChildren().addAll(navBox);
+
+        addButton.setOnAction(actionEvent -> {
+            DataHandler.reloadButtonList(buttons, buttonGridPane, deleteButton);
+            getChildren().clear();
+            getChildren().add(addButtonBox);
+        });
+
+        cancelButton.setOnAction(actionEvent -> {
+            getChildren().clear();
+            getChildren().add(navBox);
+        });
+
+        saveButton.setOnAction(actionEvent -> {
+            if (Objects.equals(inputButtonName.getText(), "") || Objects.equals(inputButtonUrl.getText(), "")) {
+                ErrorPopup errorPopup = new ErrorPopup();
+                Stage errorStage = new Stage();
+                try {
+                    errorPopup.start(errorStage);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                DataHandler.reloadButtonList(buttons, buttonGridPane, deleteButton);
+                inputButtonName.setText("");
+                inputButtonUrl.setText("");
+                getChildren().clear();
+                getChildren().add(navBox);
+            }
+        });
+    }
+}
