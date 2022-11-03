d9ea77aa67b671f12ab8f1515d93025f2ff681cd
francescoryu <francescoryu04@gmail.com>
2022-11-03 19:00:49 +0100

    started with datepicker implementation

---
+++ src/main/java/ch/francescoryu/hapin/TestApp.java
@@ -0,0 +1,28 @@
+package ch.francescoryu.hapin;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TestApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        DatePicker datePicker = new DatePicker();
+        datePicker.show();
+        Button button = new Button();
+        button.setOnAction(actionEvent -> {
+            System.out.println(datePicker.getEditor().getText());
+        });
+        VBox textBox = new VBox();
+        textBox.getChildren().addAll(button, datePicker);
+        Scene scene = new Scene(textBox);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
